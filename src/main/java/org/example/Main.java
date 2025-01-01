cd601d890a41462f8a9d1db60976958399d3b7e1
rustemov4 <azamat.rustemov.02@mail.ru>
2025-01-02 00:45:02 +0500

    Revert "added merge sort"
    This reverts commit 4d3c5a3da4de3bcab685b1ffb755c28d34ea57b2.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -20,6 +20,7 @@ public class Main {
                     swapped = true;
                 }
             }
+            // If no two elements were swapped in the inner loop, break
             if (!swapped) break;
         }
     }
@@ -58,63 +59,5 @@ public class Main {
         arr[high] = temp;
 
         return i + 1;
-    }
-
-    public static void mergeSort(int[] arr, int left, int right) {
-        if (left < right) {
-            // Find the middle point
-            int mid = left + (right - left) / 2;
-
-            // Recursively sort first and second halves
-            mergeSort(arr, left, mid);
-            mergeSort(arr, mid + 1, right);
-
-            // Merge the sorted halves
-            merge(arr, left, mid, right);
-        }
-    }
-
-    public static void merge(int[] arr, int left, int mid, int right) {
-        // Sizes of the two subarrays
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
-
-        // Temporary arrays
-        int[] leftArray = new int[n1];
-        int[] rightArray = new int[n2];
-
-        // Copy data to temp arrays
-        for (int i = 0; i < n1; i++)
-            leftArray[i] = arr[left + i];
-        for (int j = 0; j < n2; j++)
-            rightArray[j] = arr[mid + 1 + j];
-
-        // Merge the temp arrays
-        int i = 0, j = 0;
-        int k = left;
-        while (i < n1 && j < n2) {
-            if (leftArray[i] <= rightArray[j]) {
-                arr[k] = leftArray[i];
-                i++;
-            } else {
-                arr[k] = rightArray[j];
-                j++;
-            }
-            k++;
-        }
-
-        // Copy remaining elements of leftArray, if any
-        while (i < n1) {
-            arr[k] = leftArray[i];
-            i++;
-            k++;
-        }
-
-        // Copy remaining elements of rightArray, if any
-        while (j < n2) {
-            arr[k] = rightArray[j];
-            j++;
-            k++;
-        }
     }
 }
